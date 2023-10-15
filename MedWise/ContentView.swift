78448f2b4b50bc4896c40bd02e7aaa54c7d799f2
pawan779 <pawan.dharel77@gmail.com>
2023-10-15 19:15:07 -0400

    initial commit

---
+++ MedWise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MedWise
+//
+//  Created by Pawan Dharel on 10/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MedWise/MedWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedWiseApp.swift
+//  MedWise
+//
+//  Created by Pawan Dharel on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MedWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
